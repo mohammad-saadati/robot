80b40e176dd666874c9e34cf2013d5ac968a064c
Mohammad Saadati <32834340+mohammad-saadati@users.noreply.github.com>
2025-04-20 13:05:38 +0330

    chore: config prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,27 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    files: ['src/**/*.ts'],
+    files: ["src/**/*.ts"],
     languageOptions: {
-        parser: tseslint.parser,
-        parserOptions: {
-          projectService: true,
-          tsconfigRootDir:  './tsconfig.json'
-        },
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: "./tsconfig.json",
       },
-      rules: {
-          // This rule will catch reassignment of const variables
-          'no-const-assign': 'error',
-          
-          // Additional TypeScript-specific rules
-          '@typescript-eslint/no-unused-vars': 'error',
-          '@typescript-eslint/no-explicit-any': 'error',
-        },
     },
+    rules: {
+      // This rule will catch reassignment of const variables
+      "no-const-assign": "error",
+
+      // Additional TypeScript-specific rules
+      "@typescript-eslint/no-unused-vars": "error",
+      "@typescript-eslint/no-explicit-any": "error",
+    },
+  },
 );
